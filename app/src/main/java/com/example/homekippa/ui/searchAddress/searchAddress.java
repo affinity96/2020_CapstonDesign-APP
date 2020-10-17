05977903af1f6ee81bb85499e8d673de280bce50
MoHyeonMIn <mhmrm@ajou.ac.kr>
2020-10-18 00:39:17 +0900

    모현민-그룹생성하면 UI 코드 작성 - 팝업 오류 수정중

--- app/src/main/java/com/example/homekippa/ui/searchAddress/searchAddress.java
+++ app/src/main/java/com/example/homekippa/ui/searchAddress/searchAddress.java
@@ -13,6 +13,7 @@ import android.webkit.WebChromeClient;
 
 import android.webkit.WebView;
 
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,7 +31,6 @@ public class searchAddress extends AppCompatActivity {
 
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
@@ -58,15 +58,16 @@ public class searchAddress extends AppCompatActivity {
         String address = daum_result.getText().toString().trim();
     }
 
+
     public void init_webView() {
 
         // WebView 설정
+
 
         daum_webView = (WebView) findViewById(R.id.daum_webview);
 
 
         // JavaScript 허용
-
         daum_webView.getSettings().setJavaScriptEnabled(true);
 
 
@@ -75,20 +76,21 @@ public class searchAddress extends AppCompatActivity {
         daum_webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 
 
+
+
         // JavaScript이벤트에 대응할 함수를 정의 한 클래스를 붙여줌
 
-        daum_webView.addJavascriptInterface(new AndroidBridge(), "TestApp");
+        daum_webView.addJavascriptInterface(new AndroidBridge(), "Android");
 
 
         // web client 를 chrome 으로 설정
 
-        daum_webView.setWebChromeClient(new WebChromeClient());
+        daum_webView.setWebViewClient(new WebViewClient());
 
 
         // webview url load. php 파일 주소
 
         daum_webView.loadUrl("http://101.101.208.180:3000/searchAddress.html");
-
     }
 
 
