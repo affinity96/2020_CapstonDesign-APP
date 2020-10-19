c248826be0685418e438ac54265baa4d8bb2e801
affinity96 <affinity96@gmail.com>
2020-10-19 18:51:31 +0900

    이은창-회원가입화면UI코드작성, 반려동물추가화면UI코드작성 - 회원가입화면, 로그인화면 XML변경, 반려동물 추가화면 UI 코드 일부 작성

--- app/src/main/java/com/example/homekippa/LoginActivity.java
+++ app/src/main/java/com/example/homekippa/LoginActivity.java
@@ -1,32 +1,26 @@
 package com.example.homekippa;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -37,6 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser curUser;
     private TextView gotoSignTextview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +44,7 @@ public class LoginActivity extends AppCompatActivity {
         editTextPW = findViewById(R.id.editText_PW);
         buttonLogin = findViewById(R.id.button_Login);
         gotoSignTextview=findViewById(R.id.textView_gosignup);
+
 
 
         //버튼이 클릭되면 여기 리스너로 옴
@@ -69,6 +65,8 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
     public void requestLogin(String ID, String PW){
         mAuth.signInWithEmailAndPassword(ID, PW)
--- app/src/main/java/com/example/homekippa/MainActivity.java
+++ app/src/main/java/com/example/homekippa/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         Intent intent = getIntent();
         curUser = (FirebaseUser)intent.getExtras().get("user");
         Toast.makeText(getApplicationContext(), curUser.getEmail() + "님 로그인", Toast.LENGTH_LONG).show();
@@ -34,5 +36,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
+
+
 
 }
