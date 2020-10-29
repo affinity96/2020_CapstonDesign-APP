0d3e6608a20f917e7f65fef8f923f49efa071f82
MoHyeonMIn <mhmrm@ajou.ac.kr>
2020-10-29 20:20:50 +0900

    모현민- 그룹생성 화면 - 수정중

--- app/src/main/java/com/example/homekippa/CreateGroupActivity.java
+++ app/src/main/java/com/example/homekippa/CreateGroupActivity.java
@@ -124,21 +124,18 @@ public class CreateGroupActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent)
-    {
-        if(resultCode != RESULT_OK)
-        {
-            Log.d("주소", "error");
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (resultCode != RESULT_OK) {
+//            Log.d("주소", "error");
             return;
-        }
-        else{
-            if(requestCode == 0) {
+        } else {
+            if (requestCode == 0) {
                 //수신성공 출력
                 String result = intent.getStringExtra("address");
                 moveToSearchAddress.setText(result);
-            }
-            else{
-                try{
+            } else {
+                try {
 //                    InputStream in = getContentResolver().openInputStream(intent.getData());
 //                    Bitmap img = BitmapFactory.decodeStream(in);
 //                    imgUrl = intent.getData();
@@ -148,8 +145,7 @@ public class CreateGroupActivity extends AppCompatActivity {
 //                    groupCoverPhoto.setImageBitmap(img);
 //                    Uri selectedImageUri=intent.getData();
 //                    groupCoverPhoto.setImageURI(selectedImageUri);
-                }catch(Exception e)
-                {
+                } catch (Exception e) {
                     Log.d("exception", String.valueOf(e));
                 }
             }
