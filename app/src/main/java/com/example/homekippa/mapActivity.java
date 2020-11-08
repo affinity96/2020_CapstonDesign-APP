f85e7ce10a10119633ea63e61b85983eed8f47f9
MoHyeonMIn <mhmrm@ajou.ac.kr>
2020-11-08 22:50:44 +0900

    모현민 - 카카오맵 UI 화면 구현 - 카카오 맵 띄우는 중

--- app/src/main/java/com/example/homekippa/mapActivity.java
+++ app/src/main/java/com/example/homekippa/mapActivity.java
@@ -1,6 +1,7 @@
 package com.example.homekippa;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,19 +13,15 @@ public class mapActivity extends AppCompatActivity {
 
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
+        Log.d("here", "123456789");
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
         MapView mapView = new MapView(this);
 
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
-
-
-
-
-
-
 
     }
 
