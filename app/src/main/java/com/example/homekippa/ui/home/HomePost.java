70e317f968168649b2af95bd7a5ab5e2cdb568e4
LeeSiEun <skvkffish@gmail.com>
2020-11-23 02:15:57 +0900

    이시은 - 게시  - 오류 _해결

--- app/src/main/java/com/example/homekippa/ui/home/HomePost.java
+++ app/src/main/java/com/example/homekippa/ui/home/HomePost.java
@@ -96,7 +96,6 @@ public class HomePost extends Fragment {
         if (!isGroupCreated()) {
             setPostListView(listView_posts);
         }
-
         return root;
     }
 
@@ -114,8 +113,7 @@ public class HomePost extends Fragment {
 
                     postViewModel = new ViewModelProvider(requireActivity()).get(PostViewModel.class);
 
-//                    setImageData();
-
+                    setImageData();
                     ArrayList<Boolean> checkLikeList = setLikeData(likeList);
                     postViewModel.getPostList().setValue(postList);
                     postViewModel.getLikeCheck().setValue(checkLikeList);
@@ -136,7 +134,7 @@ public class HomePost extends Fragment {
 
     private void setPostAdapter(RecyclerView listView, ArrayList<Boolean> checkLikeList) {
         //Setting Sample Image Data
-        postAdapter = new ListPostAdapter(getActivity(), postList, groupList, checkLikeList, false);
+        postAdapter = new ListPostAdapter(getActivity(), (ArrayList<SingleItemPost>) postViewModel.getPostList().getValue(), groupList, checkLikeList, false);
         listView.setAdapter(postAdapter);
 
         LinearLayoutManager pLayoutManager = new LinearLayoutManager(getActivity());
@@ -145,20 +143,17 @@ public class HomePost extends Fragment {
         listView.setItemAnimator(new DefaultItemAnimator());
     }
 
-//    private void setImageData() {
-//
-//        //TODO: Change the sample Image Data!!!!!!
-//        //Setting Sample Image Data
-//        ArrayList<SingleItemPostImage> post_ImageList = new ArrayList<>();
-//        SingleItemPostImage postImage = new SingleItemPostImage(R.drawable.dog_tan);
-//        post_ImageList.add(postImage);
-//        postImage = new SingleItemPostImage(R.drawable.dog_woong);
-//        post_ImageList.add(postImage);
-//
-//        for (SingleItemPost sit : postList) {
-//            sit.setGroupPostImage(post_ImageList);
-//        }
-//    }
+    private void setImageData() {
+
+        //TODO: Change the sample Image Data!!!!!!
+        //Setting Sample Image Data
+        for (SingleItemPost p : postList) {
+            ArrayList<SingleItemPostImage> post_ImageList = new ArrayList<>();
+            SingleItemPostImage postImage = new SingleItemPostImage(p.getImage());
+            post_ImageList.add(postImage);
+            p.setGroupPostImage(post_ImageList);
+        }
+    }
 
     private ArrayList<Boolean> setLikeData(List<List<LikeData>> likeList) {
 
