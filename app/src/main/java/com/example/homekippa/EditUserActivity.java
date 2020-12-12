6c869f414bd865d61596e407a63e8085c1152688
affinity96 <affinity96@gmail.com>
2020-12-12 23:49:43 +0900

    이은창 - 회원정보수정-UI코드수정

--- app/src/main/java/com/example/homekippa/EditUserActivity.java
+++ app/src/main/java/com/example/homekippa/EditUserActivity.java
@@ -1,17 +1,22 @@
 package com.example.homekippa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.homekippa.data.EditDailyWorkData;
 import com.example.homekippa.data.UserData;
 import com.example.homekippa.network.RetrofitClient;
 import com.example.homekippa.network.ServiceApi;
+import com.example.homekippa.ui.datepicker.DatePickerFragment;
 
 public class EditUserActivity extends AppCompatActivity {
     private ServiceApi service;
@@ -41,6 +46,9 @@ public class EditUserActivity extends AppCompatActivity {
         phone_login = (EditText) this.findViewById(R.id.editText_Phone);
         birth_login = (TextView) this.findViewById(R.id.text_Birth);
         name_login = (EditText) this.findViewById(R.id.editText_Name);
+        button_gotoEditUser = (Button) this.findViewById(R.id.button_gotoEditUser);
+
+
 
         String id = userData.getUserId();
         String name = userData.getUserName();
@@ -53,5 +61,49 @@ public class EditUserActivity extends AppCompatActivity {
         birth_login.setText(birth);
         name_login.setText(name);
 
+        userEmail = email_login.getText();
+        userName = name_login.getText();
+        userPhone = phone_login.getText();
+
+
+        birth_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePicker(view);
+            }
+        });
+
+
+        button_gotoEditUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("아", "머냐ㅐ");
+
+               // editUser(new EditDailyWorkData(id, dailyWorkName.toString(), dailyWorkDesc.toString(), dailyWorkTime.toString(), dailyWorkAlarm.toString()));
+
+            }
+        });
+
+
+
+
+
     }
+
+
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month + 1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (year_string + "-" + month_string + "-" + day_string);
+
+        birth_login.setText(dateMessage);
+    }
+
+
 }
