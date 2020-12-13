e63c5bac51014cb1d7ffbaa548631473621b1453
affinity96 <affinity96@gmail.com>
2020-12-14 01:59:40 +0900

    이은창 - 일과등록ui작성 - 일부수정

---
+++ app/src/test/java/com/example/homekippa/CreateDailyWorkActivityTest.java
@@ -0,0 +1,12 @@
+//package com.example.homekippa;
+//
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+//public class CreateDailyWorkActivityTest {
+//
+//    @Test
+//    public void onCreate() {
+//    }
+//}
